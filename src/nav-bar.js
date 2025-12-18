3c5a15a65727ceea30aa928b2f09c4fb6a2d40be
HeyJunN <105594922+HeyJunN@users.noreply.github.com>
2025-12-18 22:25:41 +0900

    design: desktop/about-feature publishing, hotfix: 뷰포트 조정 시 사이드 바 보임 방지

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -11,3 +11,16 @@ openMenuBtn.addEventListener("click", () => {
 closeMenuBtn.addEventListener("click", () => {
   navBar.classList.remove("active");
 });
+
+/* 리사이즈 시 트랜지션 방지 코드 */
+let resizeTimer;
+window.addEventListener("resize", () => {
+  // 1. 창 크기가 변할 때 transition-none 클래스 추가 (애니메이션 끔)
+  document.body.classList.add("resize-animation-stopper");
+
+  // 2. 리사이즈가 끝나면 (타이머로 감지) 클래스 제거 (애니메이션 다시 켬)
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400); // 0.4초 뒤에 다시 켬
+});
