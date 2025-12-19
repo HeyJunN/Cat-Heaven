7b5d10388ee990bb3224dc81ffa663f3eb9a13bd
HeyJunN <105594922+HeyJunN@users.noreply.github.com>
2025-12-19 18:13:49 +0900

    chore: og-image 절대 경로 수정

--- src/scroll-top.js
+++ src/scroll-top.js
@@ -1,13 +1,12 @@
 const scrollTopBtn = document.querySelector(".scroll-top-btn");
 const footer = document.querySelector("footer");
 
-// 1. 스크롤 탑 버튼 보이기/숨기기 로직
+
 window.addEventListener("scroll", () => {
   // footer 위치 확인
   const footerRect = footer.getBoundingClientRect();
   const windowHeight = window.innerHeight;
 
-  // Footer가 화면에 조금이라도 보이면 버튼이 나타남
   // (footerRect.top < windowHeight) 조건은 Footer의 윗부분이 화면 아래쪽 경계선을 넘어왔다는 뜻
   if (footerRect.top < windowHeight - 50) {
     // 50px 정도 여유 둠
@@ -16,7 +15,7 @@ window.addEventListener("scroll", () => {
     scrollTopBtn.classList.remove("show");
   }
 
-  // 2. 클릭 시 최상단 이동 + 회전 애니메이션
+  // 클릭 시 최상단 이동 + 회전 애니메이션
   scrollTopBtn.addEventListener(
     "click",
     () => {
